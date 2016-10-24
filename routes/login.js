a2ffc50e91c7ef2dd0f33ea7834d0196b8c062c5
yujitoyama <yuji.toyama@gmail.com>
2016-10-24 18:59:26 +0900

    20161024_r5

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,39 @@
  * login home page.
  */
 
+//【トップ画面遷移機能/ログインチェック機能】
 exports.login = function(req, res){
-  res.render('top', { title: 'Crawler top' });
+	
+	//DB接続	  
+	var connection = db.dbinit();		
+	connection.connect(function(err) {
+		  if (err) {
+		    console.error('error connecting: ' + err.stack);
+		    return;
+		  }
+		  console.log('connected as id ' + connection.threadId);
+		});
+
+//SQL準備		
+	var sql = "select * from crawler.user where userid = ? and password = ?";
+	
+//SQL実行		
+	var query = connection.query(sql, [req.body.userid, req.body.userpassword
+	                                   ],function (err, rows) {
+		if (err) {
+			console.log(err);
+			connection.rollback();
+		} else 
+			if(rows.length==1){
+			res.render('top', { 
+				title: 'top' 	
+			})}
+			else{
+			res.render('loginerr',{
+				title: 'loginerr'
+			})	
+			};
+			});
+//DB切断		
+	db.dbend();   
 };
